aa94eeabb73837f1f6cd3e22985b0bec6e638daf
Thomas Crha <thomas@9bitbyte.com>
2023-09-03 17:46:21 +1000

    moved autonma logic into canvas for simplicity

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from rich.style import Style
 from textual.binding import Binding
 from textual.geometry import Offset, Region
 from textual.app import App, ComposeResult
-import time
 from textual.strip import Strip
 from textual.widget import Widget
 from textual.widgets import Footer
@@ -12,41 +11,11 @@ from textual.reactive import var
 import random
 
 
-class CellularAutomaton:
-    def __init__(self, canvas_matrix: list[list[int]]) -> None:
-        self.canvas_matrix = canvas_matrix
-        self.CANVAS_HEIGHT = len(canvas_matrix)
-        self.CANVAS_WIDTH = len(canvas_matrix[0])
-
-    def get_neighbours(self, x: int, y: int) -> list[int]:
-        """Get the neighbours of a cell."""
-        neighbours = []
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if i == j == 0:
-                    continue
-                neighbours.append(self.canvas_matrix[(y + i) % self.CANVAS_HEIGHT][(x + j) % self.CANVAS_WIDTH])
-        return neighbours
-
-    def get_next_generation(self) -> list[list[int]]:
-        """Get the next generation of the canvas."""
-        new_canvas_matrix = [[0 for _ in range(self.CANVAS_WIDTH)] for _ in range(self.CANVAS_HEIGHT)]
-        for y in range(self.CANVAS_HEIGHT):
-            for x in range(self.CANVAS_WIDTH):
-                neighbours = self.get_neighbours(x, y)
-                if self.canvas_matrix[y][x] == 1:
-                    if 2 <= sum(neighbours) <= 3:
-                        new_canvas_matrix[y][x] = 1
-                else:
-                    if sum(neighbours) == 3:
-                        new_canvas_matrix[y][x] = 1
-        return new_canvas_matrix
-
 class Canvas(Widget):
     COMPONENT_CLASSES: set = {
         "canvas--white-square",
         "canvas--black-square",
-        "canvas--cursor-square"
+        "canvas--cursor-square",
     }
 
     DEFAULT_CSS: str = """
@@ -72,7 +41,10 @@ class Canvas(Widget):
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.canvas_matrix = [[0 for _ in range(self.CANVAS_WIDTH + 1)] for _ in range(self.CANVAS_HEIGHT + 1)]
+        self.canvas_matrix = [
+            [0 for _ in range(self.CANVAS_WIDTH + 1)]
+            for _ in range(self.CANVAS_HEIGHT + 1)
+        ]
         self.cellular_automaton = CellularAutomaton(self.canvas_matrix)
 
         self.running = False
@@ -81,19 +53,80 @@ class Canvas(Widget):
 
     def action_clear(self) -> None:
         print("clear")
-        self.canvas_matrix = [[0 for _ in range(self.CANVAS_WIDTH + 1)] for _ in range(self.CANVAS_HEIGHT + 1)]
+        self.canvas_matrix = [
+            [0 for _ in range(self.CANVAS_WIDTH + 1)]
+            for _ in range(self.CANVAS_HEIGHT + 1)
+        ]
         self.refresh()
 
+    # step methods
     def action_step(self) -> None:
         print("step")
-        self.canvas_matrix = self.cellular_automaton.get_next_generation()
+        self.canvas_matrix = self.get_next_generation()
         self.refresh()
+
+    def get_neighbours(self, x: int, y: int) -> list[int]:
+        """Get the neighbours of a cell."""
+        neighbours = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == j == 0:
+                    continue
+                neighbours.append(
+                    self.canvas_matrix[(y + i) % self.CANVAS_HEIGHT][
+                        (x + j) % self.CANVAS_WIDTH
+                    ]
+                )
+        return neighbours
+
+    def get_next_generation(self) -> list[list[int]]:
+        """Get the next generation of the canvas."""
+        new_canvas_matrix = [
+            [0 for _ in range(self.CANVAS_WIDTH + 1)]
+            for _ in range(self.CANVAS_HEIGHT + 1)
+        ]
+        for y in range(self.CANVAS_HEIGHT):
+            for x in range(self.CANVAS_WIDTH):
+                neighbours = self.get_neighbours(x, y)
+                if self.canvas_matrix[y][x] == 1:
+                    if 2 <= sum(neighbours) <= 3:
+                        new_canvas_matrix[y][x] = 1
+                else:
+                    if sum(neighbours) == 3:
+                        new_canvas_matrix[y][x] = 1
+        return new_canvas_matrix
 
     def action_random(self) -> None:
         print("random")
         for y in range(self.CANVAS_HEIGHT):
             for x in range(self.CANVAS_WIDTH):
                 self.canvas_matrix[y][x] = random.randint(0, 1)
+        self.refresh()
+
+    def action_decrease_canvas(self) -> None:
+        print("decrease")
+        if self.CANVAS_HEIGHT <= 10 or self.CANVAS_WIDTH <= 10:
+            return
+
+        self.CANVAS_HEIGHT -= 10
+        self.CANVAS_WIDTH -= 10
+        self.canvas_matrix = [
+            [0 for _ in range(self.CANVAS_WIDTH + 1)]
+            for _ in range(self.CANVAS_HEIGHT + 1)
+        ]
+        self.refresh()
+
+    def action_increase_canvas(self) -> None:
+        print("increase")
+        if self.CANVAS_HEIGHT >= 100 or self.CANVAS_WIDTH >= 100:
+            return
+
+        self.CANVAS_HEIGHT += 10
+        self.CANVAS_WIDTH += 10
+        self.canvas_matrix = [
+            [0 for _ in range(self.CANVAS_WIDTH + 1)]
+            for _ in range(self.CANVAS_HEIGHT + 1)
+        ]
         self.refresh()
 
     @property
@@ -116,17 +149,26 @@ class Canvas(Widget):
     def on_mouse_move(self, event: events.MouseMove) -> None:
         """Called when the user moves the mouse over the widget."""
         mouse_position = event.offset + self.scroll_offset
-        self.cursor_square = Offset(mouse_position.x // self.ROW_HEIGHT, mouse_position.y // int(self.ROW_HEIGHT / 2))
+        self.cursor_square = Offset(
+            mouse_position.x // self.ROW_HEIGHT,
+            mouse_position.y // int(self.ROW_HEIGHT / 2),
+        )
 
     def on_click(self, event: events.Click) -> None:
         """Called when the user clicks on the widget."""
         mouse_position = event.offset + self.scroll_offset
-        self.cursor_square = Offset(mouse_position.x // self.ROW_HEIGHT, mouse_position.y // int(self.ROW_HEIGHT / 2))
+        self.cursor_square = Offset(
+            mouse_position.x // self.ROW_HEIGHT,
+            mouse_position.y // int(self.ROW_HEIGHT / 2),
+        )
         self.x = self.cursor_square.x
         self.y = self.cursor_square.y
 
         # toggle the square
-        if len(self.canvas_matrix) > self.y and len(self.canvas_matrix[self.y]) > self.x:
+        if (
+            len(self.canvas_matrix) > self.y
+            and len(self.canvas_matrix[self.y]) > self.x
+        ):
             self.canvas_matrix[self.y][self.x] ^= 1
 
         self.refresh(self.information_bar_region)
@@ -135,7 +177,12 @@ class Canvas(Widget):
     def get_square_region(self, square_offset: Offset) -> Region:
         """Get region relative to widget from square coordinate."""
         x, y = square_offset
-        region = Region(x * self.ROW_HEIGHT, y * int(self.ROW_HEIGHT / 2), self.ROW_HEIGHT, int(self.ROW_HEIGHT / 2))
+        region = Region(
+            x * self.ROW_HEIGHT,
+            y * int(self.ROW_HEIGHT / 2),
+            self.ROW_HEIGHT,
+            int(self.ROW_HEIGHT / 2),
+        )
         # Move the region in to the widgets frame of reference
         region = region.translate(-self.scroll_offset)
         return region
@@ -167,8 +214,15 @@ class Canvas(Widget):
             else:
                 square_style = self.black
                 # only update the scauare that aren't out of range
-                if len(self.canvas_matrix) > row and len(self.canvas_matrix[row]) > column:
-                    square_style = self.black if self.canvas_matrix[row][column] == 1 else self.white
+                if (
+                    len(self.canvas_matrix) > row
+                    and len(self.canvas_matrix[row]) > column
+                ):
+                    square_style = (
+                        self.black
+                        if self.canvas_matrix[row][column] == 1
+                        else self.white
+                    )
 
             return square_style
 
@@ -183,10 +237,11 @@ class Canvas(Widget):
 class CellularAutomatonTui(App):
     BINDINGS = [
         Binding("s", "step", "Step"),
-        Binding("q", "quit", "Quit"),
-        Binding("c", "clear", "Clear", priority=True),
+        Binding("+", "increase_canvas", "Increase canvas size"),
+        Binding("-", "decrease_canvas", "Decrease canvas size"),
         Binding("r", "random", "Random"),
-        Binding("p", "pause", "Pause")
+        Binding("c", "clear", "Clear", priority=True),
+        Binding("q", "quit", "Quit"),
     ]
 
     def compose(self) -> ComposeResult:
@@ -196,6 +251,12 @@ class CellularAutomatonTui(App):
 
     def action_quit(self) -> None:
         exit()
+
+    def action_increase_canvas(self) -> None:
+        self.canvas.action_increase_canvas()
+
+    def action_decrease_canvas(self) -> None:
+        self.canvas.action_decrease_canvas()
 
     def action_clear(self) -> None:
         self.canvas.action_clear()
@@ -207,8 +268,6 @@ class CellularAutomatonTui(App):
         self.canvas.action_random()
 
 
-
 if __name__ == "__main__":
     app = CellularAutomatonTui()
     app.run()
-
